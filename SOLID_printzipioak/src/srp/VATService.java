211780644171da79d327d3bc7a6e21c722fad722
Egoitz <eladron002@ikasle.ehu.eus>
2025-10-30 16:14:31 +0100

    SRP zuzenketa VATService klasean

--- SOLID_printzipioak/src/srp/VATService.java
+++ SOLID_printzipioak/src/srp/VATService.java
@@ -3,7 +3,7 @@ package srp;
 public class VATService {
 	private double percentage = 0.16;
 	public float calculateVAT(float amount, String code) {
-		if(code.equals("0")) {
+		if(!code.equals("0")) {
 			return (float) (amount*percentage);	
 		} else {
 			return 0;
